b14f5076128d5bd22c21422d89dde67743a28813
radcliffeo <43155857+radcliffeo@users.noreply.github.com>
2018-09-24 23:56:39 -0500

    Update labOne.cpp

--- Desktop/COSC 2030/Lab 1/labOne.cpp
+++ Desktop/COSC 2030/Lab 1/labOne.cpp
@@ -7,13 +7,18 @@ int main() {
   double checkEnd;
   int i = 4;
   bool endFile = false;
-
+  
+  // make a file stream object and open it
   ifstream infile;
   infile.open("num3.txt");
 
+  // go ahead and get the first two values
   infile >> first >> second;
+  
+  // assign the last two so we can do checks on them
   infile >> secondLast >> last;
-
+  
+  // update the last two values and total count until end of file
   while (infile >> checkEnd) {
     if (infile.fail()) { break; } else {
       secondLast = last;
@@ -22,12 +27,14 @@ int main() {
     }
   }
 
+  // print results
   cout << "First value: " << first << endl;
   cout << "Second value: " << second << endl;
   cout << "Second to last value: " << secondLast << endl;
   cout << "Last value: " << last << endl;
   cout << "Total number of values: " << i;
 
+  // close file and end program
   infile.close();
   return 0;
 }
